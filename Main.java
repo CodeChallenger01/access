bf101221105896fd5ed8705f7d319038a5732efe
Manish Mishra <124979629+ManishKnolder@users.noreply.github.com>
2023-02-20 21:34:46 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
  public static void main(String args[]){
-    System.out.println("Hello form Manish cccas a Software Intern");
+    System.out.println("Hello form Manish a Software Intern");
    
   }
  }
